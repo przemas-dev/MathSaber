e542d560ea64c08c9765520dcff33efbd21b0ca3
TrylMik <64104285+TrylMik@users.noreply.github.com>
2023-01-23 02:27:08 +0100

    delay between stages

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,17 +10,21 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public List<XmlNodeList> questionsList;
+    public List<Question> questions;
+    public Queue<Question> Questions = new();
     public QuestionManager QuestionManager;
     public Question ActiveQuestion;
-    public float DistanceBetweenQuestioin = 10f;
-    public float Velocity = 2f;
-    public float TimeBetweenQuestion => DistanceBetweenQuestioin / Velocity;
-    public Queue<Question> Questions = new();
-    List<Question> questions;
     public TextMeshPro QuestionLabel;
     public TextMeshPro PointsLabel;
     public bool delayBetweenStages = false;
-    private int _points;
+    public bool endOfGame = false;
+    public float DistanceBetweenQuestioin = 10f;
+    public float Velocity = 2f;
+    public float TimeBetweenQuestion => DistanceBetweenQuestioin / Velocity;
+    public float TimeBeforeStage = 10.0f;
+    public int currentStage;
+    public int absoluteNumberOfStages = 3;
+    public int startingNumberOfStage = 3;
     public int Points
     {
         get => _points;
@@ -31,41 +35,46 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    public int currentStage;
-
-
-
+    private int _points;
+    private float _delayTime = 0;
     private float _timeSinceLastQuestion = 0;
+
     void Start()
     {
-        currentStage = 2;
-        questions = Stage(currentStage);
         StartNewGame();
     }
 
     private void StartNewGame()
     {
-        ActiveQuestion = QuestionManager.SpawnNewQuestion(Velocity, questions, currentStage);
-        QuestionLabel.text = ActiveQuestion.QuestionText;
+        endOfGame = false;
+        currentStage = startingNumberOfStage;
+        questions = Stage(currentStage);
+        delayBetweenStages = true;
         Points = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (questions.Count <= 0)
-        {
-            currentStage++;
-            questions = Stage(currentStage);
-        }
+        EndOfStage();
 
-        _timeSinceLastQuestion += Time.deltaTime;
-        if (_timeSinceLastQuestion >= TimeBetweenQuestion)
+        CountingTime();
+        
+
+        if (_timeSinceLastQuestion >= TimeBetweenQuestion && currentStage <= absoluteNumberOfStages)
         {
             Questions.Enqueue(QuestionManager.SpawnNewQuestion(Velocity, questions, currentStage));
             _timeSinceLastQuestion = 0f;
         }
-           
+
+        if(_delayTime >= TimeBeforeStage && currentStage <= absoluteNumberOfStages)
+        {
+            ActiveQuestion = QuestionManager.SpawnNewQuestion(Velocity, questions, currentStage);
+            QuestionLabel.text = ActiveQuestion.QuestionText;
+            _delayTime = 0;
+            delayBetweenStages = false;
+        }
+
     }
 
     private void IncreaseSpeed(float value = 0.5f)
@@ -93,6 +102,47 @@ public class GameManager : MonoBehaviour
         ActiveQuestion = Questions.Dequeue();
         QuestionLabel.text = ActiveQuestion.QuestionText;
     }
+
+    public void ShowStageNumber()
+    {
+        if(endOfGame)
+            QuestionLabel.text = $"Thanks for playing!";
+        else
+            QuestionLabel.text = $"Stage {currentStage}";
+    }
+
+    public void CountingTime()
+    {
+        if (delayBetweenStages)
+        {
+            _delayTime += Time.deltaTime;
+
+            if (_delayTime >= Velocity)
+                ShowStageNumber();
+        }
+        else
+        {
+            _timeSinceLastQuestion += Time.deltaTime;
+        }
+    }
+
+    private void EndOfStage()
+    {
+        if (questions.Count <= 0 && currentStage <= absoluteNumberOfStages) 
+        {
+            currentStage++;
+            questions = Stage(currentStage);
+            delayBetweenStages = true;
+        }
+
+        if (currentStage > absoluteNumberOfStages)
+        {
+            endOfGame = true;
+            delayBetweenStages = true;
+        }
+
+    }
+
 
     public List<Question> Stage(int currentStage)
     {
